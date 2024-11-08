5b050fae5b1998a14d4ee1e6ae157c7221921d57
Nishant Rai <n.rai.code@outlook.com>
2024-11-08 23:26:57 +0530

    navbar scrollbar effect added

--- src/app/(landing)/_components/Navbar.tsx
+++ src/app/(landing)/_components/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Logo from './Logo'
+import { cn } from '@/lib/utils'
+import { useScrollTop } from '@/hooks/use-scroll-top'
 
 function Navbar() {
+    const scrolled = useScrollTop();
     return (
-        <div className='flex justify-between'>
+        <div className={cn(
+            "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+            scrolled && "border-b shadow-sm"
+        )}>
             <Logo />
             Navbar
         </div>
